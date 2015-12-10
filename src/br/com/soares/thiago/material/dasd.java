afc5b41065b2a862983065391e36b51119b13a58
Thiago Batista Soares <thiagobsoares@globomail.com>
2015-12-10 12:39:24 -0200

    Arrumando os conflitos de configurações

---
+++ src/br/com/soares/thiago/material/dasd.java
@@ -0,0 +1,13 @@
+package src.br.com.soares.thiago.material;
+
+public class dasd {
+	
+	public static int xa(long x, int j){
+		
+	}
+	
+	public static void main(String[] args) {
+		int sa = 1;
+		xa(sa);
+	}
+}
