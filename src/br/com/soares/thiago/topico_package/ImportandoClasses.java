f6738e71e69e82f9727ced81fe2da7c510302bb0
Thiago Batista Soares <thiagobsoares@globomail.com>
2015-12-14 03:55:07 -0200

    Colocando o README e algumas classes novas

---
+++ src/br/com/soares/thiago/topico_package/ImportandoClasses.java
@@ -0,0 +1,29 @@
+/* 
+Para colocar uma classe em um pacote, segue o exemplo,
+Cada . no package, significa um  novo diretório
+
+*/
+
+package br.com.soares.thiago.topico_package;
+
+/*
+Podemos usar as classes  de outros pacotes por dois métodos:
+
+* Usar o import no inicio do código (depois do package se existir)
+	-Podemos importar apenas uma classe do pacote ( java.util.ArrayList )
+	-Ou podemos importa o pacote todo ( java.util.* )
+	
+* O outro jeito de usar classe de outros pacotes, chamar ela no meio do código pelo nome full
+(java,util.ArrayList lista =  new java.util.ArrayList();) Lógicamente, pouco ineficiente desse jeito.
+
+*/
+
+import java.util.HashMap;	//Está ok
+import java.util.*;	//Está ok
+import java.util.HashMap.*; // Compila normalmente, mas o HashMap não é importado.
+import java.util.hashMap; // Não compila, para import a regra de case sensitive é válida.
+
+/*
+Por padrão o pacote JAVA.LANG já é importado, O ÚNICO pacote que é importado automaticamente
+*/
+
---
+++ src/br/com/soares/thiago/variavel/InicializandoVarivael.java
@@ -0,0 +1,7 @@
+package br.com.soares.thiago.variavel;
+
+public class InicializandoVarivael {
+	short x = 9 * 3;
+	byte b = 2 * 31;
+	float y = 2.0f;
+}
