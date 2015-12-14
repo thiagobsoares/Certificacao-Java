401ce32db54192bc43729cead907e72a3283da43
Thiago Batista Soares <thiagobsoares@globomail.com>
2015-12-14 12:09:39 -0200

    Rebase

--- src/br/com/soares/thiago/topico_package/ExecutandoClasse.java
+++ src/br/com/soares/thiago/topico_package/ExecutandoClasse.java
@@ -2,6 +2,6 @@ package br.com.soares.thiago.topico_package;
 
 public class ExecutandoClasse {
 	public static void main(String[] args) {
-		System.out.println("Ola Mundo");
+		System.out.println("Ola Brasil");
 	}
 }
