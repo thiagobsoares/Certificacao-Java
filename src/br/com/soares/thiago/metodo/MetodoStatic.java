a31d5791f3aadd7f0a1a5b7a9620a367dbec067c
Thiago Batista Soares <thiagobsoares@globomail.com>
2015-12-14 12:15:45 -0200

    Comitando o pacote de métodos

---
+++ src/br/com/soares/thiago/metodo/MetodoStatic.java
@@ -0,0 +1,10 @@
+package br.com.soares.thiago.metodo;
+
+public class MetodoStatic {
+	public static int x = 10;
+	public static double d = 10f;
+
+	public static void main(String[] args) {
+		System.out.println(x + d);
+	}
+}
