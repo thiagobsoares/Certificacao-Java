6f2127ed0e4000130b4f968215a5f994ae1ce743
Thiago Batista Soares <thiagobsoares@globomail.com>
2015-12-14 12:12:42 -0200

    Colocando mais duas classe!

---
+++ src/br/com/soares/thiago/topico_package/TestandoImplementacao.java
@@ -0,0 +1,7 @@
+package br.com.soares.thiago.topico_package;
+
+public class TestandoImplementacao {
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo, classe controle!");
+	}
+}
--- src/br/com/soares/thiago/variavel/InicializandoVarivael.java
+++ src/br/com/soares/thiago/variavel/InicializandoVarivael.java
@@ -4,4 +4,5 @@ public class InicializandoVarivael {
 	short x = 9 * 3;
 	byte b = 2 * 31;
 	float y = 2.0f;
+	double d = 2312.1 + 2f;
 }
